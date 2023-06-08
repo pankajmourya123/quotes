ba93289b36cfe7a4e6c546ec2cd005aa419093fc
pankajmourya123 <pankajmourya5000@gmail.com>
2023-06-08 19:15:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  // http://api.quotable.io/random
+
+
+  useEffect(() => {
+    fetchNewQuote();
+  },[]);
+
+  const  fetchNewQuote = () => {
+    axios.get("http://api.quotable.io/random")
+     .then(res =>{
+      setQuote(res.data.content)
+      setAuthor(res.data.author)
+     }).catch(err=>{
+      console.log(err)
+     })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote">
+        <h2>{quote}</h2>
+        <small>-{author}-</small>
+      </div>
+      <br />
+      <button className="btn" onClick={fetchNewQuote}>
+        Generate New Quote
+      </button>
     </div>
   );
 }
